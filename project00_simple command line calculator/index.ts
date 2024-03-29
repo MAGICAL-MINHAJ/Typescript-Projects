5ed09f22d90f36c4194397d448629cc6d1f847a9
Minhaj Khan <160640040+MAGICAL-MINHAJ@users.noreply.github.com>
2024-03-29 22:29:41 +0500

    add files via upload

---
+++ project00_simple command line calculator/index.ts
@@ -0,0 +1,51 @@
+#! /usr/bin/env node
+
+ import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+{
+type: 'number',
+name: 'num1',
+message:'enter the first number: '
+
+},
+{
+
+    type: 'number',
+    name: 'num2',
+    message: 'enter the second number: '
+
+},
+{
+
+    type: 'list',
+    name: 'operator',
+    message: 'select an operator: ',
+    choices: ['add','subtract','multiply','divide']
+
+}
+]);
+
+let result: number;
+
+switch(answer.operator){
+
+    case 'add':
+        result=answer.num1+answer.num2;
+        console.log("The answer for addition is: "+ result);
+        break;
+
+    case 'subtract':
+        result=answer.num1-answer.num2;
+        console.log("The answer for subtraction is: "+ result);
+        break;
+
+    case 'multiply':
+        result=answer.num1*answer.num2;
+        console.log("The answer for multiplication is: "+ result);
+        break;
+
+    case 'divide':
+        result=answer.num1/answer.num2;
+        console.log("The answer for division is: "+ result);
+        break;
+}
